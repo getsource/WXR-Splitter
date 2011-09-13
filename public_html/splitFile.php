223f1a4373401ab8176b48930bbaddf6cd07e713
Mike Schroder <mschrode@umich.edu>
2011-09-12 19:45:27 -0700

    splitFile.php working with local files, still EXTREMELY experimental/dirty.

--- public_html/splitFile.php
+++ public_html/splitFile.php
@@ -1,29 +1,49 @@
 <?php
 
-$choppedPress_cmd = "/home/wxrsplitter/wxrsplitter.getsource.net/choppedPress.py";
-$output_path = "/home/wxrsplitter/wxrsplitter.getsource.net/tmp/";
+$CHOPPEDPRESS_CMD = "/home/wxrsplitter/wxrsplitter.getsource.net/choppedPress.py";
+$OUTPUT_PATH = "/home/wxrsplitter/wxrsplitter.getsource.net/public_html/splitFiles/";
 
 if ($_FILES["file"]["error"] > 0)
   {
-  echo "Error: " . $_FILES["file"]["error"] . "<br />";
+  echo "Error: " . $_FILES["file"]["error"] . "<br />\n";
   }
 else
   {
-  echo "Upload: " . $_FILES["file"]["name"] . "<br />";
-  echo "Type: " . $_FILES["file"]["type"] . "<br />";
-  echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
-  echo "Stored in: " . $_FILES["file"]["tmp_name"];
+  echo "Upload: " . $_FILES["file"]["name"] . "<br />\n";
+  echo "Type: " . $_FILES["file"]["type"] . "<br />\n";
+  echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Mb<br />\n";
+  echo "Stored in: " . $_FILES["file"]["tmp_name"] . "<br />\n\n";
   }
 
+
 $fileToConvert = $_FILES["file"]["tmp_name"];
+$fileSize = $_FILES["file"]["size"] / (1024*1024);
+
+$outputFile = md5( rand() + time() );
 
 
+
+
+echo "FileToConvert Contents: '$fileToConvert'";
 // Add Default file to Convert for testing
 if ( !$fileToConvert ) {
 	$fileToConvert = "/home/wxrsplitter/testFile.xml";
+
+	echo "Copying '$fileToConvert' to '$OUTPUT_PATH$outputFile.xml'\n";
+	
+	if ( !copy($fileToConvert, "$OUTPUT_PATH$outputFile.xml") )
+		die( "File copy Failed!" );
+		
+	$fileSize = filesize($fileToConvert) / (1024*1024);
+	echo "FileSize: $fileSize";
 }
 
-$commandToRun = "python $choppedPress_cmd  -i  $fileToConvert -o $output_path$fileToConvert";
+
+$numChunks = (int)$fileSize;
+
+move_uploaded_file($fileToConvert, "$OUTPUT_PATH$outputFile.xml");
+
+$commandToRun = "python $CHOPPEDPRESS_CMD -n $numChunks -i $OUTPUT_PATH$outputFile.xml -o $OUTPUT_PATH$outputFile";
 
 echo "Running $commandToRun ...";
 $output = shell_exec( $commandToRun );
